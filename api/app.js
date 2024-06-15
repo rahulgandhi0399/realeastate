3fc45bd1ba6175411f62752b8d1484bf41c9d931
Rahul <Rahul2.gandhi@ril.com>
2024-06-15 21:01:31 +0530

    first commit

---
+++ api/app.js
@@ -0,0 +1,37 @@
+import path from "path";
+import  express  from "express";
+import  cors  from "cors";
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js"
+import authRoute from "./routes/auth.route.js"
+import userRoute from "./routes/user.route.js"
+import chatRoute from "./routes/chat.route.js"
+import messageRoute from "./routes/message.route.js"
+const app = express();
+
+const __dirname = path.resolve();
+
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoute)
+app.use("/api/post",postRoute)
+app.use("/api/users",userRoute)
+app.use("/api/chats",chatRoute)
+app.use("/api/messages",messageRoute)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
+}
+
+
+
+app.listen(8800,()=>{
+    console.log("Server is running!"); 
+})
